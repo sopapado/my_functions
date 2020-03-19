25dfb16267412e77fc2d18fc4fcdc509d6719492
sopapado <sotocpa@gmail.com>
2020-03-19 14:59:22 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='my_functions',
+      version='1.0',
+      description='A collection of photonics and optics functions',
+      url='http://github.com/sopapado/my_functions',
+      author='Sotirios Papadopoulos',
+      author_email='send@git.com',
+      license='MIT',
+      packages=['my_functions'],
+      zip_safe=False)
+      
